8679cae2e34de3e0c522a3c115df9d144264c519
Misael Villar <villarmisael7@gmail.com>
2025-09-01 18:38:55 -0600

    feat: implementar funcionalidad de búsqueda de recursos - Crear SearchResourceViewModel para lógica de búsqueda - Diseñar layout fragment_search_resource.xml con AppBar y toolbar - Actualizar SearchResourceFragment con nueva funcionalidad - Mejorar layout fragment_edit_resource.xml - Añadir strings necesarios para búsqueda de recursos - Configurar navegación y toolbar para pantalla de búsqueda

---
+++ app/src/main/java/com/devvillar/resourl/features/searchresource/presentation/viewmodels/SearchResourceViewModel.kt
@@ -0,0 +1,7 @@
+package com.devvillar.resourl.features.searchresource.presentation.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class SearchResourceViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
